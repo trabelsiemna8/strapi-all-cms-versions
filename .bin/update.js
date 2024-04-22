760baeeed374d76b181cc8de36f7b79e7d65296f
Florent Baldino <florent.baldino@strapi.io>
2024-04-22 18:23:26 +0200

    Init version 4.21.1

--- .bin/update.js
+++ .bin/update.js
@@ -2,7 +2,7 @@ const fs = require("node:fs");
 const path = require("node:path");
 const { exec, execSync } = require("node:child_process");
 
-const MIN_VERSION = "4.4.0";
+const MIN_VERSION = "4.21.1";
 const REPO_ROOT = path.join(__dirname, "..");
 const INSTALL_BATCH_SIZE = 12;
 const INSTALL_TIMEOUT = 15000; // ms - Exit install after timeout to skip dependency installation
@@ -129,7 +129,9 @@ const moveVersionsToBranches = (versions) => {
   for (const version of versions) {
     const strapiPath = path.join(REPO_ROOT, "workdir", version);
 
-    if (!fs.existsSync(strapiPath)) continue;
+    if (!fs.existsSync(strapiPath)) {
+      console.log(`Skipping version ${version} as it was not installed`);
+    }
 
     console.log(`\n> Creating branch for ${version}`);
     git("checkout master");
@@ -154,7 +156,7 @@ const moveVersionsToBranches = (versions) => {
 const main = async () => {
   const versions = listStrapiVersions({ minVersion: MIN_VERSION });
 
-  await installStrapiVersions(versions);
+  // await installStrapiVersions(versions);
   moveVersionsToBranches(versions);
 
   console.log("\nDone.");
